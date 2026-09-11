acc5892ec899b84a943f1c35ddf04b7320f54303
Claudio <claferci@gmail.com>
2026-09-11 18:30:44 +0200

    Crear tablas automaticamente al iniciar

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from basedatos import obtener_cuentas, obtener_movimientos, obtener_presupuesto, obtener_categorias, obtener_recurrentes, insertar_movimiento
+from basedatos import crear_tablas
+crear_tablas()
 
 st.set_page_config(page_title="Dashboard", page_icon="🏠")
 
